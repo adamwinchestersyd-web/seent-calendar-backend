a293db625cf4bd7b57f6025caa5dd605c03b8c87
Adam Winchester <adamwinchester.syd@gmail.com>
2025-11-07 09:12:04 +1100

    Fix: Using env variable for API URL

--- src/hooks/useCases.ts
+++ src/hooks/useCases.ts
@@ -8,7 +8,7 @@ export function useCases() {
   useEffect(() => {
     async function load() {
       try {
-        const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+        const API_URL = import.meta.env.VITE_API_URL;
         const r = await fetch(`${API_URL}/api/cases`);
         if (!r.ok) throw new Error(await r.text());
         const data = await r.json();
