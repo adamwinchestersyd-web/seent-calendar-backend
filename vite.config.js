011c04e525205ec902556646da77db2674266b88
Adam Winchester <adamwinchester.syd@gmail.com>
2025-11-07 12:54:49 +1100

    feat: Change build directory to dist-v2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  // When running `vite`/`npm run dev` keep base at '/',
-  // but when building for cPanel subfolder /seent/, point assets there.
   base: command === 'build' ? '/' : '/',
+  build: {
+    outDir: 'dist-v2'
+  } 
 }))
