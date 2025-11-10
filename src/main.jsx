2a02250b39fa47fa6ef236b6cb3820a4130c21cb
Adam Winchester <adamwinchester.syd@gmail.com>
2025-11-10 12:06:38 +1100

    Brute Force Attempt #2

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
-// Load BOTH global Tailwind utilities and the calendar theme.
 import "./index.css";
-import "./app/ui/calendar.css";
-import './app/ui/manualEventStyle.css'
-
-// Single toast provider for the entire app.
-import { ToastProvider } from "./app/utils/useToast";
+// import './app/ui/manualEventStyle.css' // <-- DELETE OR COMMENT OUT THIS LINE
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+    <App />
   </React.StrictMode>
 );
